367f0d75f2d974a83a52f123431030b4979231fd
Philani Ncube <ncbphi001@gmail.com>
2022-05-02 23:27:52 +0200

    test

--- pages/index.jsx
+++ pages/index.jsx
@@ -1,17 +1,10 @@
-import React, { Fragment, Suspense, useState } from 'react';
+import React, { Fragment, Suspense } from 'react';
 import { useRouter } from 'next/router';
-import SignIn from '../components/SignIn';
 import { useUser } from '../context/AuthContext';
-import { supabase } from '../utils/supabase';
-import cookie from 'cookie';
-import Loading from '../components/Loading';
-import getProfile from '../lib/getProfile';
 import { getEvents } from '../lib/getEvents';
 import { dehydrate, QueryClient, useQuery } from 'react-query';
 import { getSchools } from '../lib/getSchools';
-import Header from '../components/Header';
 import EventsFeed from '../components/Events/EventsFeed';
-import EventSkeleton from '../components/Loaders/EventSkeleton';
 
 const Home = ({ message = 'Please Sign In' }) => {
   const { user, signIn } = useUser();
@@ -42,11 +35,6 @@ Home.headerTitle = 'Home';
 export default Home;
 
 export async function getServerSideProps({ req }) {
-  const { user } = await supabase.auth.api.getUserByCookie(req);
-  const token = cookie.parse(req.headers.cookie)['sb-access-token'];
-
-  supabase.auth.session = () => ({ access_token: token });
-
   const queryClient = await new QueryClient();
 
   await queryClient.prefetchQuery('schools', getSchools);
